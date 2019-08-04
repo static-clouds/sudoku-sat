1b082895bc8bd1868d8b6e6d8512844f2a8f0f39
C A <consolation.archaism@protonmail.com>
2019-08-04 19:21:17 +0100

    add quickcheck test for dpll/eval

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.QuickCheck
+
+import CNF
+import DPLL(dpll)
+import EvalCNF(isValidCNF)
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_dpllMakesCNFValid
+
+prop_dpllMakesCNFValid :: CNF -> Bool
+prop_dpllMakesCNFValid cnf = case dpll cnf of
+  Just cnf' -> isValidCNF cnf'
+  Nothing   -> True
