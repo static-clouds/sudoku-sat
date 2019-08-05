8a3512c784fa9c7b0ea08da7de03f7bfc6b42fb5
C A <consolation.archaism@protonmail.com>
2019-08-05 14:03:54 +0100

    add a simple test for a single value as a sanity check

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,18 +3,29 @@ import Test.QuickCheck
 import CNF
 import DPLL(dpll)
 import EvalCNF(isValidCNF)
+import Data.Maybe (fromJust, isJust)
+import qualified Data.Set as Set
 
 
 main :: IO ()
 main = do
   quickCheck prop_dpllMakesCNFValid
-  -- quickCheck prop_eval
+  quickCheck prop_dpllMakesCNFValidSimple
 
 prop_dpllMakesCNFValid :: CNF -> Bool
 prop_dpllMakesCNFValid cnf = case dpll cnf of
-  Just cnf' -> isValidCNF cnf'
+  -- Can the result of dpll cnf be used to solve cnf?
+  Just cnf' -> isValidCNF (combine cnf cnf')
   Nothing   -> True
 
 
-prop_eval :: CNF -> Bool
-prop_eval cnf = isValidCNF cnf
+genSimpleCNF :: Gen CNF
+genSimpleCNF = elements $ [ cnf [ disj [ p "a"]
+                                , disj [ n "a", n "b"]
+                                ]
+                          ]
+
+prop_dpllMakesCNFValidSimple :: Gen Bool
+prop_dpllMakesCNFValidSimple = do
+  cnf <- genSimpleCNF
+  pure $ isJust $ dpll cnf
