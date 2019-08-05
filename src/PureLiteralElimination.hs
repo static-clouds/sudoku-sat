eb37a4a13285be9aa60caf7446671818c21896fc
C A <consolation.archaism@protonmail.com>
2019-08-05 14:02:42 +0100

    don't delete singletons/unit lits when doing pure literal elimination

--- src/PureLiteralElimination.hs
+++ src/PureLiteralElimination.hs
@@ -5,12 +5,15 @@ import qualified Data.Set as Set
 
 import CNF (CNF(..), Clause(..), Lit(..), allLiterals, posAtoms, negAtoms)
 
-updateLiterals :: (Set.Set Lit -> Set.Set Lit) -> Clause -> Clause
-updateLiterals f (Disj lits) = Disj (f lits)
+deleteUnlessSingleton :: Lit -> Clause -> Clause
+deleteUnlessSingleton lit (Disj lits) = Disj newLits
+  where newLits
+          | Set.size lits == 1 = lits
+          | otherwise          = Set.delete lit lits
 
 eliminateLiteral :: Lit -> CNF -> CNF
 eliminateLiteral lit (CNF clauses) = CNF $ Set.map updateClause clauses
-  where updateClause = updateLiterals $ Set.delete lit
+  where updateClause = deleteUnlessSingleton lit
 
 allPureLiterals :: CNF -> Set.Set Lit
 allPureLiterals cnf = purePosLiterals `Set.union` pureNegLiterals
