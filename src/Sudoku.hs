0ac2be32762fc9c69370031bbb1f819d6fe6ec46
C A <consolation.archaism@protonmail.com>
2019-08-14 19:21:06 +0100

    use xor to express constraints on sudoku

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -16,7 +16,7 @@ toVal '-' = Nothing
 toVal s   = Just (read [s] :: Int)
 
 toMoves :: String -> [SudokuCellAtom]
-toMoves s = [(C { row = row, col = col, val = fromJust val}) | (row, col) <- allPositions gridSize
+toMoves s = [(C { row = row, col = col, val = (fromJust val) - 1}) | (row, col) <- allPositions gridSize
                                                              , let val' = board !! row !! col
                                                              , let val = toVal val'
                                                              , isJust val
@@ -69,8 +69,8 @@ setVal cell val = cell { val = val }
 cellValues :: [Int] -> SudokuCellAtom -> [SudokuCellAtom]
 cellValues values cell = map (setVal cell) values
 
-cellRule :: [Int] -> SudokuCellAtom -> [Lit SudokuCellAtom]
-cellRule values cell@(C { val = val }) = makeRule (matchesVal val) $ cellValues values cell
+cellRule :: [Int] -> SudokuCellAtom -> [[Lit SudokuCellAtom]]
+cellRule values cell = makeXorRule $ cellValues values cell
 
 setCol :: SudokuCellAtom -> Int -> SudokuCellAtom
 setCol cell col = cell { col = col }
@@ -81,8 +81,8 @@ rowValues values cell = map (setCol cell) values
 matchesCol :: Int -> SudokuCellAtom -> Bool
 matchesCol col (C { col = col' }) = col' == col
 
-rowRule :: [Int] -> SudokuCellAtom -> [Lit SudokuCellAtom]
-rowRule values cell@(C { col = col }) = makeRule (matchesCol col) $ rowValues values cell
+rowRule :: [Int] -> SudokuCellAtom -> [[Lit SudokuCellAtom]]
+rowRule values cell@(C { col = col }) = makeXorRule $ rowValues values cell
 
 matchesRow :: Int -> SudokuCellAtom -> Bool
 matchesRow row (C { row = row' }) = row' == row
@@ -93,8 +93,8 @@ setRow cell row = cell { row = row }
 colValues :: [Int] -> SudokuCellAtom -> [SudokuCellAtom]
 colValues values cell = map (setRow cell) values
 
-colRule :: [Int] -> SudokuCellAtom -> [Lit SudokuCellAtom]
-colRule values cell@(C { row = row }) = makeRule (matchesRow row) $ colValues values cell
+colRule :: [Int] -> SudokuCellAtom -> [[Lit SudokuCellAtom]]
+colRule values cell@(C { row = row }) = makeXorRule $ colValues values cell
 
 box' :: Int -> Int -> Int -> [(Int, Int)]
 box' boxSize r_ c_ = [(r, c) | r <- [rowStart..rowEnd], c <- [colStart..colEnd]]
@@ -119,16 +119,17 @@ boxValues values cell = map (setRowCol cell) $ values
 matchesRowCol :: Int -> Int -> SudokuCellAtom -> Bool
 matchesRowCol row col cell@(C { row = row', col = col' } ) = row == row' && col == col'
 
-boxRule :: Int -> SudokuCellAtom -> [Lit SudokuCellAtom]
-boxRule gridSize cell@(C { row = row, col = col }) = makeRule (matchesRowCol row col) $ boxValues (box gridSize cell) cell
+boxRule :: Int -> SudokuCellAtom -> [[Lit SudokuCellAtom]]
+boxRule gridSize cell = makeXorRule $ boxValues (box gridSize cell) cell
 
 sudokuCnf :: Int -> CNF SudokuCellAtom
 sudokuCnf gridSize = CNF $ Set.fromList clauses
   where
-    params = [C { row = row, col = col, val = val } | row <- values, col <- values, val <- values]
     values = gridSpan gridSize
+    params = [C { row = row, col = col, val = val } | row <- values, col <- values, val <- values]
     rules = [cellRule values, rowRule values, colRule values, boxRule gridSize]
-    clauses = [clauseFromSet $ Set.fromList $ rule param | rule <- rules, param <- params]
+    clauses = map (clauseFromSet . Set.fromList) clauseLists
+    clauseLists = concat [rule param | rule <- rules, param <- params]
 
 easyData   = "7-6-9--8-X-----69--X98-5-2-7-X312-4---5X---153---X4---6-318X-6-8-9-31X--73-----X-4--2-8-7"
 mediumData = "--6-3--5-X------2-8X-8--95--4X934---527X----4----X815---649X2--81--9-X4-1------X-6--7-3--"
